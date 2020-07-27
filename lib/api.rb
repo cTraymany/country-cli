9eda724007cb61ba463b40c01d10ae5753c0cf6e
cTraymany <chindalatht@gmail.com>
2020-07-27 19:36:26 +0700

    specifies and creates methods for api key and endpoint url

--- lib/api.rb
+++ lib/api.rb
@@ -1,4 +1,16 @@
+# http request - GET https://api.nasa.gov/planetary/apod
+
 class API
+    @@api = "20200727193003"
+    @@url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
+
+    def self.api
+        @@api
+    end
+
+    def self.url
+        @@url
+    end
 
     def self.get_photo
     end
--- lib/cli.rb
+++ lib/cli.rb
@@ -4,7 +4,12 @@ class CLI
         # puts "welcome message and app intro here"
     end
 
+    # def get_photo
+    #     API.get_photo
+    # end
 
-
+    # def get_photo_by_date(date)
+    #     API.get_photo_by_date(date)
+    # end
 
 end
