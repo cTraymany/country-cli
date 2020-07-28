bc3d79fd4f8da4f9b45d76e42c62d371c7f4a595
cTraymany <chindalatht@gmail.com>
2020-07-28 21:58:33 +0700

    pretties up the CLI

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,6 +1,7 @@
 class CLI
 
     def self.run
+        puts ""
         puts "Welcome! Please enter the calling code for the coutry you would like to learn about."
         self.get_country_by_calling_code
     end
@@ -10,10 +11,12 @@ class CLI
         
         if API.valid_country?(calling_code.to_i)
             countries = API.extract_country(calling_code.to_i)
+            puts ""
             puts "This country code returns:"
             Country.get_country_from_array(countries)
             self.get_country(calling_code)
         else
+            puts ""
             puts "This code does not have a country!"
             puts "Please enter a valid country code."
             self.get_country_by_calling_code
@@ -26,12 +29,15 @@ class CLI
     def self.get_country(calling_code)
         countries_data = API.get_country_by_calling_code(calling_code.to_i) # returns an array of nested country hashes
 
+        puts ""
         puts "Please enter the country number you would like to see."
         input = gets.chomp
+        #############ADD ERROR HANDLING HERE
         
         new_country = Country.new(countries_data[input.to_i - 1])
         binding.pry
         
+        puts ""
         puts "You chose #{new_country.name}!"
         puts "#{new_country.name} has a population of #{new_country.population}."
         puts "It is located in the region of #{new_country.region}, and its capital is #{new_country.capital}."
