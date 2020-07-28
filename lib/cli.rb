d6925b44ebe3ff70f4fb216ec749fb9a5b58e7eb
cTraymany <chindalatht@gmail.com>
2020-07-28 22:37:16 +0700

    maps areas needing control flow

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,11 +3,14 @@ class CLI
     def self.run
         puts ""
         puts "Welcome! Please enter the calling code for the coutry you would like to learn about."
+        puts "To exit, enter 'exit' at any time."
         self.get_country_by_calling_code
     end
     
     def self.get_country_by_calling_code
         calling_code = gets.chomp
+
+        ##############ENTER CONTROL FLOW FOR EXIT
         
         if API.valid_country?(calling_code.to_i)
             countries = API.extract_country(calling_code.to_i)
@@ -28,35 +31,41 @@ class CLI
 
 
     def self.get_country(calling_code)
-        countries_data = API.get_country_by_calling_code(calling_code.to_i) # returns an array of nested country hashes
+        # while calling_code != 'exit'
 
-        puts ""
-        puts "Please enter the number you would like to see."
-        
-        input = gets.chomp
-        input = input.to_i
-        
-        # binding.pry
-        if (1..countries_data.length).include?(input)
-            new_country = Country.new(countries_data[input.to_i - 1])
-        else
+
+
+
+
+            countries_data = API.get_country_by_calling_code(calling_code.to_i) # returns an array of nested country hashes
+
             puts ""
-            puts "Your entry is invalid!"
-            self.get_country(calling_code)
-        end
+            puts "Please enter the number you would like to see."
+            
+            input = gets.chomp
+            input = input.to_i
+            
+            # binding.pry
+            if (1..countries_data.length).include?(input)
+                new_country = Country.new(countries_data[input.to_i - 1])
+            else
+                puts ""
+                puts "Your entry is invalid!"
+                self.get_country(calling_code)
+            end
 
-        # binding.pry
-        
-        puts ""
-        puts ""
-        puts "You chose #{new_country.name}!"
-        puts ""
-        puts "#{new_country.name} has a population of #{new_country.population}."
-        puts ""
-        puts "It is located in the region of #{new_country.region}, and its capital is #{new_country.capital}."
-        puts ""
-        puts "View a photo of #{new_country.name}'s country flag at #{new_country.flag_link}."
-        puts ""
+            # binding.pry
+            
+            puts ""
+            puts ""
+            puts "You chose #{new_country.name}!"
+            puts ""
+            puts "#{new_country.name} has a population of #{new_country.population}."
+            puts ""
+            puts "It is located in the region of #{new_country.region}, and its capital is #{new_country.capital}."
+            puts ""
+            puts "View a photo of #{new_country.name}'s country flag at #{new_country.flag_link}."
+            puts ""
         
         
     end
